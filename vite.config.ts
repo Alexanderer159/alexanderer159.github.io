bd7c81b513666a4a48b24a4169a1478ef710676a
Alejandro De Yavorsky <aledeyavorsky@gmail.com>
2026-01-08 18:53:44 +0100

    Start over

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/alexanderer159.github.io/",
   server: {
     host: "::",
     port: 8080,
