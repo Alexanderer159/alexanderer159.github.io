dad588464f129e8c96b7483acf77cb173227712e
Alejandro De Yavorsky <aledeyavorsky@gmail.com>
2026-01-08 18:26:14 +0100

    Start over

--- src/components/Footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import "./footer.css"
-
-export const Footer = () => (
-
-	<div className="footer d-flex justify-content-center align-items-center">
-			<p className="m-0">© 2025 Alejandro Dev.</p>
-	</div>
-);
